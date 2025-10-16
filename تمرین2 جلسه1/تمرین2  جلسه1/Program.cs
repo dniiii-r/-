36b5083c053b46da60d52ebd0a68bacb36f7ee94
dniiii-r <danial.r2005@gmail.com>
2025-10-16 12:34:15 +0330

    Add files via upload

---
+++ تمرین2 جلسه1/تمرین2  جلسه1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+
+            Console.Write("Please enter the temperature in Celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("Equivalent temperature in Fahrenheit = " + fahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
