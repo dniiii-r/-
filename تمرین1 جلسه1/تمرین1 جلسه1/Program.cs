76daa63855f762c3d57596a7aa7970e7e2efda31
dniiii-r <danial.r2005@gmail.com>
2025-10-16 12:30:23 +0330

    Add files via upload

---
+++ تمرین1 جلسه1/تمرین1 جلسه1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, area, perimeter;
+            const double pi = 3.14;
+
+            Console.Write("Please enter the radius of the circle: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            area = pi * r * r;
+            perimeter = 2 * pi * r;
+
+            Console.WriteLine("Area of the circle = " + area);
+            Console.WriteLine("Perimeter of the circle = " + perimeter);
+
+            Console.ReadKey();
+        }
+    }
+}
