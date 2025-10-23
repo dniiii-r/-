dd2c27337276f4cf1e6a64b24cb23de8b7617627
dniiii-r <danial.r2005@gmail.com>
2025-10-23 20:56:41 +0330

    Add files via upload

---
+++ تمرین2 جلسه2/تمرین2 جلسه2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 7 == 0)
+        {
+            Console.WriteLine("YES!");
+        }
+        else
+        {
+            int result = number * 3;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
