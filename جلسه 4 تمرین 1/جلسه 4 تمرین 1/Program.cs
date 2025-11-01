e6717ce72b1de9fe8711f6f82c04d7fba3182d77
dniiii-r <danial.r2005@gmail.com>
2025-11-01 16:31:43 +0330

    Add files via upload

---
+++ جلسه 4 تمرین 1/جلسه 4 تمرین 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        Console.WriteLine("Even numbers: " + even);
+        Console.WriteLine("Odd numbers: " + odd);
+
+        Console.ReadKey();
+    }
+}
