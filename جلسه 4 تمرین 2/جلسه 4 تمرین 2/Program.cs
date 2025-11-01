4c9a096334f3383dab85bbcc9a96d861edf1f2b7
dniiii-r <danial.r2005@gmail.com>
2025-11-01 16:34:32 +0330

    Add files via upload

---
+++ جلسه 4 تمرین 2/جلسه 4 تمرین 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int position = -1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Enter number " + (i + 1) + ": ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 2 && position == -1)
+            {
+                position = i + 1;
+            }
+        }
+
+        if (position != -1)
+            Console.WriteLine("Number 2 is found at position: " + position);
+        else
+            Console.WriteLine("Number 2 is not found");
+
+            Console.ReadKey();
+    }
+}
