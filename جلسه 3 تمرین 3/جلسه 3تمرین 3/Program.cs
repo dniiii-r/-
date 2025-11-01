019f7b0c56f41c369b2d157f4e3eac3af5c94448
dniiii-r <danial.r2005@gmail.com>
2025-11-01 16:29:09 +0330

    Add files via upload

---
+++ جلسه 3 تمرین 3/جلسه 3تمرین 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+        int product = 1;
+
+        while (true)
+        {
+            Console.Write("Enter a number (0 to stop): ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+                break;
+
+            product *= number;
+        }
+
+        Console.WriteLine("Product = " + product);
+    }
+}
