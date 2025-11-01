c4a4dcc0e93227fb4a9171ff43cbe631501cdd99
dniiii-r <danial.r2005@gmail.com>
2025-11-01 16:27:22 +0330

    Add files via upload

---
+++ جلسه 3 تمرین 2/جلسه 3 تمرین 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Letter E
+        Console.WriteLine("*****");
+        Console.WriteLine("*");
+        Console.WriteLine("*****");
+        Console.WriteLine("*");
+        Console.WriteLine("*****");
+        Console.WriteLine();
+
+        // Letter H
+        Console.WriteLine("*   *");
+        Console.WriteLine("*   *");
+        Console.WriteLine("*****");
+        Console.WriteLine("*   *");
+        Console.WriteLine("*   *");
+        Console.WriteLine();
+
+        // Letter I
+        Console.WriteLine("*****");
+        Console.WriteLine("  *  ");
+        Console.WriteLine("  *  ");
+        Console.WriteLine("  *  ");
+        Console.WriteLine("*****");
+    }
+}
