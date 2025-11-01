892607b6b41cb7747f6b391b1df070064425affd
dniiii-r <danial.r2005@gmail.com>
2025-11-01 16:25:16 +0330

    Add files via upload

---
+++ جلسه 3 تمرین 1/جلسه 3 تمرین 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number (1-7): ");
+        int day = int.Parse(Console.ReadLine());
+
+        if (day == 1)
+            Console.WriteLine("Saturday");
+        else if (day == 2)
+            Console.WriteLine("Sunday");
+        else if (day == 3)
+            Console.WriteLine("Monday");
+        else if (day == 4)
+            Console.WriteLine("Tuesday");
+        else if (day == 5)
+            Console.WriteLine("Wednesday");
+        else if (day == 6)
+            Console.WriteLine("Thursday");
+        else if (day == 7)
+            Console.WriteLine("Friday");
+        else
+            Console.WriteLine("END!");
+
+            Console.ReadKey();
+    }
+}
