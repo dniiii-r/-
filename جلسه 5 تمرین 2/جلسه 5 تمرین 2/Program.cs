29cf06f9a4a043ca9616abb77cd3c5b5f3f37767
dniiii-r <danial.r2005@gmail.com>
2025-11-11 15:52:32 +0330

    Add files via upload

---
+++ جلسه 5 تمرین 2/جلسه 5 تمرین 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static int Multiply(int a, int b)
+    {
+        if (b == 0)
+            return 0;
+        else if (b > 0)
+            return a + Multiply(a, b - 1);
+        else
+            return -Multiply(a, -b);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Multiplication = " + Multiply(a, b));
+        Console.ReadKey();
+    }
+}
