52cd94e73bb6a002aec71186b07c131819ed6f2e
dniiii-r <danial.r2005@gmail.com>
2025-11-11 15:48:23 +0330

    Add files via upload

---
+++ جلسه 5 تمرین 1/جلسه 5 تمرین 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static double Perimeter(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    static double Area(double a, double b, double c)
+    {
+        double p = (a + b + c) / 2;
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter side A: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side B: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side C: ");
+        double c = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Perimeter of the triangle = " + Perimeter(a, b, c));
+        Console.WriteLine("Area of the triangle = " + Area(a, b, c));
+        Console.ReadKey();
+    }
+}
